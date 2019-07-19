e923d7fd0e8523c66d34ae170df60b819c1372f4
AhmadNagra <ahnagra005@gmail.com>
2019-07-19 11:32:56 +0500

    abc

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -4,6 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 import { RegisterService } from '../register.service';
 import { EditService } from '../edit.service';
+import { RegisteredUser } from '../shared/models/registereduser.model';
 
 @Component({
   selector: 'app-table',
@@ -21,24 +22,44 @@ export class TableComponent implements OnInit {
   phoneh=false;
   jobh=false;
   fileh=false;
- 
+  displaynames:string[]=['placeholder', 'placeholder', 'placeholder','placeholder','placeholder'];
+  pagenum=1;
 
   constructor(private _registerservice:RegisterService, private _idservice:EditService) {}
 
   ngOnInit() {
     this._registerservice.ViewAll().subscribe(data => {
-      this.user = data
+      this.user = data;
+      for(var i=0;i<5;i++){
+        var tempfileName=this.user[i].fileName;
+        var tfn=tempfileName.substring(0,tempfileName.indexOf(";"));
+        var filename=tfn.substring(9);
+        this.displaynames[i]=filename;
+        //console.log(this.displaynames[i]);
+      
+      }
     });
     this._registerservice.GetCount().subscribe(data=> {
       this.TotalPages=data
       this.fakearray=new Array(Math.ceil(this.TotalPages/5))
     }) 
   }
-  
-  Download(filename:string){
-    var filepath="C:/Users/mhas/Downloads/Compressed/WebAPI/WebApiProject/Uploads/"+filename;
-    
+  Download(fn:string){
+    var tempfileName=fn;
+    var tfn=tempfileName.substring(0,tempfileName.indexOf(";"));
+    var contentT=tempfileName.substring(tempfileName.indexOf(";")+1);
+    var content=contentT.substring(contentT.indexOf(":")+1,contentT.indexOf(";"));
+    var base64=tempfileName.substring(tempfileName.indexOf(",")+1);
+    var filename=tfn.substring(9);
+    var blob=this.base64ToBlob(base64,content);
+    var url = window.URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = filename;
+    link.click();
+       
   }
+  
   Sort(value:string){
     this.nameh=false;
     this.emailh=false;
@@ -62,11 +83,26 @@ export class TableComponent implements OnInit {
     }
     this._registerservice.SortBy(value).subscribe(data => {
       this.user = data
+      for(var i=0;i<5;i++){
+        var tempfileName=this.user[i].fileName;
+        var tfn=tempfileName.substring(0,tempfileName.indexOf(";"));
+        var filename=tfn.substring(9);
+        this.displaynames[i]=filename;
+        //console.log(this.displaynames[i]);
+      
+      }
     }); 
   }
   GetPage(pageno:number){
+    this.pagenum=pageno;
     this._registerservice.GetPage(pageno).subscribe(data => {
       this.user = data
+      for(var i=0;i<5;i++){
+        var tempfileName=this.user[i].fileName;
+        var tfn=tempfileName.substring(0,tempfileName.indexOf(";"));
+        var filename=tfn.substring(9);
+        this.displaynames[i]=filename;
+      }
     });
   }
 
@@ -79,4 +115,22 @@ export class TableComponent implements OnInit {
   set_edit(id:number){
     this._idservice.set_id(id);
   }
+
+  public base64ToBlob(b64Data, contentType='', sliceSize=512) {
+    b64Data = b64Data.replace(/\s/g, ''); 
+    console.log(b64Data);
+    let byteCharacters = atob(b64Data);
+    let byteArrays = [];
+    for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+        let slice = byteCharacters.slice(offset, offset + sliceSize);
+
+        let byteNumbers = new Array(slice.length);
+        for (var i = 0; i < slice.length; i++) {
+            byteNumbers[i] = slice.charCodeAt(i);
+        }
+        let byteArray = new Uint8Array(byteNumbers);
+        byteArrays.push(byteArray);
+    }
+    return new Blob(byteArrays, {type: contentType});
+}
 }
