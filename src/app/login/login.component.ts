5abf24942f8e703443efa3237e61a3a3d2b99fdc
Rida Maryam <Rida123maryam@gmail.com>
2019-07-22 17:05:47 +0500

    Remember me functionality without any validations

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,6 +4,9 @@ import { Router } from '@angular/router';
 import { UseExistingWebDriver } from 'protractor/built/driverProviders';
 import { User } from '../classes/user';
 import { nearer } from 'q';
+import { CookieService } from 'ngx-cookie-service';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -14,20 +17,51 @@ export class LoginComponent implements OnInit {
 
   currentUser: User;
   private isValidatedUser:boolean;
-  constructor(private _loginServive: LoginService, private router: Router) { 
+  private rememberMe : boolean;
+
+  constructor(private fb: FormBuilder,private _loginServive: LoginService, private router: Router, private _cookieService: CookieService) { 
     this.isValidatedUser = false;
+    this.rememberMe = false;
+    
+
+    
+    if(_cookieService.get('remember')) {
+      var username =this._cookieService.get('username');
+      var pwd =this._cookieService.get('password');
+      this.rememberMe = true;
+      this.currentUser = new User(username,pwd);
+   } else {
+    this.currentUser = new User("","");
+   }
+  
+  
   }
    ngOnInit() {
    }
 
-   onClickSubmit(formData) {
-     this._loginServive.validateLogin(formData.username, formData.pwd).subscribe((data)=>{
+   onClickSubmit() {
+     
+     this._loginServive.validateLogin(this.currentUser.userName, this.currentUser.plainPassword).subscribe((data)=>{
       this.isValidatedUser = data;
       if (this.isValidatedUser) {
-        this.currentUser = new User(formData.username, formData.pwd);
-        this._loginServive.startNewSession(formData.username, formData.pwd);
+        console.log(this.rememberMe);
+
+
+        //cookies----
+        if (this.rememberMe) {
+          this._cookieService.set('username',this.currentUser.userName);
+          this._cookieService.set('password', this.currentUser.plainPassword);
+        } else {
+          //delete any cookie with this username and password
+          this._cookieService.delete('username',this.currentUser.userName);
+          this._cookieService.delete('password',this.currentUser.plainPassword);
+        }
+        //---------
+
+        this.currentUser = new User(this.currentUser.userName, this.currentUser.plainPassword);
+        this._loginServive.startNewSession(this.currentUser.userName, this.currentUser.plainPassword);
         this._loginServive.currentUser.subscribe(x => this.currentUser = x);
-        this._loginServive.startNewSession(formData.username,formData.pwd);
+        this._loginServive.startNewSession(this.currentUser.userName,this.currentUser.plainPassword);
         
       } else {
         alert("Username or Password is incorrect!");
