0c304669c4f59a996bc5a877f43d317c0710b8d1
Rida Maryam <Rida123maryam@gmail.com>
2019-07-26 09:58:29 +0500

    pagination achieved with API pagination function. Without stylinh

--- src/app/modules/to-do-items/to-do-items.component.ts
+++ src/app/modules/to-do-items/to-do-items.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/TodoItems/api.service';
 import { TodoItem } from '../../classes/todoItem/todo-item';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-to-do-items',
@@ -13,27 +14,47 @@ export class ToDoItemsComponent implements OnInit {
   private totalCount:number;
   private todoItems:Array<TodoItem>;
   private columns;
-  
+  private totalPages;
+  private itemsPerPage;
+  private numbers = [];
+  subscription: Subscription;
+
   constructor(private _apiService: ApiService) { 
     this.columns = ['Id','Title','IsComplete','Description','Priority','File'];
-  
+    this.itemsPerPage = 5;
   }
 
   ngOnInit() {
-    this._apiService.getCountOfAllRecords().subscribe((data)=>{
+    this.subscription = this._apiService.getCountOfAllRecords().subscribe((data)=>{
       this.totalCount = data;
-      console.log("page no got by dbtodoitems is",this.totalCount);
+      this.totalPages = Math.ceil(this.totalCount/this.itemsPerPage);
+      this.numbers = Array(this.totalPages).fill([],0).map((x,i)=>i);
+
     });
 
-      this._apiService.getRecords().subscribe((data)=>{
-        this.todoItems = data;
-        this.items = Array(this.todoItems.length).fill([], 0).map((x, i) => (this.todoItems[i]));
-    });
+    this.getDataForPagination();
+    
   }
+
+  getDataForPagination(pageNo=1) {
+        //(currentPageNo: string="", pageCapacity:string="", orderByAttribute:string="", searchForWord:string = "", searchInAttribute:string =""  )
+      this._apiService.getRecords(pageNo,this.itemsPerPage).subscribe((data)=>{
+        this.todoItems = [];
+        this.todoItems = data;
+        
+        this.items = Array(this.todoItems.length).fill([], 0).map((x, i) => (this.todoItems[i]));
+      });
+  }
+
   onChangePage(pageOfItems: Array<any>) {
     // update current page of items
     this.pageOfItems = pageOfItems;
   }
 
+
+  loadPage(pageNo) {
+    this.getDataForPagination(pageNo);
+   
+  }
  
 }
