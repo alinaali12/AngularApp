f87c2d164bcabdf0ccd9b0b3eb15a0c4d8958653
Rida Maryam <Rida123maryam@gmail.com>
2019-07-29 14:24:54 +0500

    working on validations in reset password

--- src/app/modules/reset-password/reset-password.component.ts
+++ src/app/modules/reset-password/reset-password.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/classes/user';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Location } from "@angular/common";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import custom validator to validate that password and confirm password fields match
+import { MustMatch } from '../../_helpers/must-match.validator';
 import { ResetPasswordService } from 'src/app/services/ResetPassword/reset-password.service';
 
 
@@ -10,20 +13,27 @@ import { ResetPasswordService } from 'src/app/services/ResetPassword/reset-passw
   templateUrl: './reset-password.component.html',
   styleUrls: ['./reset-password.component.scss']
 })
+
+
 export class ResetPasswordComponent implements OnInit {
   currentUser: User;
   sub:any;
   public passwordUpdated;
   public confirmPassword;
+  loginForm: FormGroup;
 
-  constructor(private router: ActivatedRoute, private routerr:Router ,location: Location, private  resetPasswordService: ResetPasswordService) { 
-    
-
+  constructor(private router: ActivatedRoute, private routerr:Router ,location: Location, private  resetPasswordService: ResetPasswordService, private formBuilder:FormBuilder) { 
     var userName = location.path().slice(location.path().search("=")+1); //we get this by slicing from the location that the first = is found,till end
     this.currentUser = new User(userName,"");
   }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+  }, {
+      validator: MustMatch('pwd', 'confirmPassword')
+  });
   }
 
   submitNewPassword() {
